a05dce2716c13ba187c0e316edf3619932a39e70
Bodhie <stefanbode1995@hotmail.com>
2018-01-16 13:46:02 +0100

    Photo dingen

---
+++ src/components/photo/photo.vue
@@ -0,0 +1,48 @@
+<template>
+     <div class="photo">
+        <h3 class="my-4 text-center text-lg-left">Picture</h3>
+
+          <div class="row text-center text-lg-left">
+
+            <div class="col-lg-3 col-md-4 col-xs-6" v-for="item in photoList" >
+              <a href="#" class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" :src="item.photo_url" :alt="item.photo_id">
+              </a>
+            </div>
+          </div>
+
+         <!-- Button trigger modal -->
+        <CreatePhotoItem></CreatePhotoItem>
+     </div>
+</template>
+
+<script>
+import CreatePhotoItem from './photoItem.vue'
+export default {
+    name: 'Photo',
+     data: function() {
+         return  {
+           photoList: []
+         }
+    },
+    created: function () {
+        this.fetchData();
+    },
+    methods: {
+        fetchData: function () {
+            this.$http.get('http://stefanbode.nl/api/photo/read.php').then(response => {
+                this.photoList = response.data.records;
+                console.log(this.photoList);
+             })
+        }
+    },
+    components: {
+        CreatePhotoItem
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/photo/photoItem.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="create-item">
+        <div class="row">
+            <div class="col-sm-12">
+                <button class="btn btn-success" data-toggle="modal" data-target="#create-photo"><i class="fa fa-plus-circle"></i></button>
+            </div>
+        </div>
+        <div id="create-photo" class="modal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Create an Photo Item</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="form-group">
+                                    <label for="start-date"><strong>Photo name:</strong></label>
+                                    <input type="field" class="form-control" id="photo" v-model="photo">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" v-on:click="savePhoto" data-dismiss="modal">Save</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    module.exports = {
+        data () {
+            return {
+                photo: ''
+            }
+        },
+        methods: {
+           savePhoto: function () {
+                this.$http.get('http://stefanbode.nl/api/photo/create.php?url='+this.photo+'&family_id=2').then(response => {
+
+                 })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .create-item{
+        text-align: default;
+    }
+</style>
