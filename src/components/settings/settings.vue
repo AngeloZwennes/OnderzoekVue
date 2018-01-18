a4ceb9aa23e0711156bb7138cd05afe8dc34b641
checkthisuzi <angelo_93@live.nl>
2018-01-18 12:44:02 +0100

    fixed everything.

--- src/components/settings/settings.vue
+++ src/components/settings/settings.vue
@@ -39,7 +39,19 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-sm-4"></div>
+                    <div class="col-sm-4">Save info</div>
+                    <div class="col-sm-8">
+                        <button @click="saveInfo()">Save information</button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">Return to dashboard</div>
+                    <div class="col-sm-8">
+                        <button @click="returnToDashboard()">Return to dashboard</button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">Log out</div>
                     <div class="col-sm-8">
                         <button @click="logOut()">Log out</button>
                     </div>
@@ -56,27 +68,46 @@
       return {
         user: {},
         family: {
+          family_id: "",
           name: ""
         },
       }
     },
     methods: {
+      returnToDashboard() {
+        this.$router.push('dashboard');
+      },
+      saveInformation() {
+        this.$http.post('https://stefanbode.nl/api/user/update.php?user_id='+ this.user.id + '&email=' + this.user.email + '&name= '+ this.user.name +
+          '&family_id='+ this.user.family_id);
+        localStorage.setItem('DbUser',this.user);
+      },
       getUser() {
         this.user = JSON.parse(localStorage.getItem('dbUser'));
+        this.getFamilyById(this.user.family_id);
       },
       logOut: function(){
         localStorage.removeItem('localUser');
         localStorage.removeItem('dbUser');
         this.$router.push('/');
       },
+      getFamilyById: function(id){
+        this.$http.get('http://stefanbode.nl/api/family/read_one.php?family_id='+ id).then(response => {
+          if(response.body.family_id === 0) {
+          }
+          this.family.name = response.body.name;
+          this.family.family_id = response.body.family_id;
+        });
+      },
       createFamily: function() {
-        console.log("TEST");
         this.$http.get('http://stefanbode.nl/api/family/create.php?name='+ this.family.name).then(response => {
           //fetch created family
-          console.log(response);
-          this.$http.get('http://stefanbode.nl/api/family/read_one.php?name='+ this.family.name).then(createdFamily => {
-            console.log(createdFamily.body);
-          })
+          this.family.family_id = response.body.family_id;
+          this.family.name = response.body.name;
+          this.user.family_id = this.family.family_id;
+          this.$http.post('http://stefanbode.nl/api/user/update.php?user_id=' + this.user.user_id + '&email=' + this.user.email +
+            '&name=' + this.user.name + '&family_id=' + response.body.family_id);
+          localStorage.setItem('dbUser',JSON.stringify(this.user));
         })
       }
     },
